0f330e21afd4370bf61095e3ffd1e4b29009d04e
NagmaKhattar <nagmakhattar@gmail.com>
2023-05-26 13:13:18 -0400

    added new branch

---
+++ src/demoweekthree/DemonstrationFriday.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demoweekthree;
+
+/**
+ * This is a new class in new branch DemoFriday
+ * @author HP
+ */
+public class DemonstrationFriday
+{
+    
+}
