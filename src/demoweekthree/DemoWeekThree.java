5507107f0a7ca563785c1357164ca57db4198ba1
NagmaKhattar <nagmakhattar@gmail.com>
2023-05-26 12:24:35 -0400

    initial commit

---
+++ src/demoweekthree/DemoWeekThree.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demoweekthree;
+
+/**
+ * This is for demonstration in Week 3
+ * @author HP
+ */
+public class DemoWeekThree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+    
+}
